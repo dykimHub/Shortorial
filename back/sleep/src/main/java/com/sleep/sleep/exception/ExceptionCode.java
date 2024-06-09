216cecda7e5fa1103476c5042643a50f23cbb4c4
ssafy10-dykim <dy.jo.kim@gmail.com>
2024-06-10 02:04:36 +0900

    Feat: 에러 추가, 에러 코드 변수 추가

---
+++ back/sleep/src/main/java/com/sleep/sleep/exception/ExceptionCode.java
@@ -0,0 +1,20 @@
+package com.sleep.sleep.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "해당되는 id의 회원이 존재하지 않습니다."),
+
+    SHORTS_NOT_FOUND(HttpStatus.NOT_FOUND, "SHORTS_001", "해당되는 id의 쇼츠가 존재하지 않습니다."),
+    ALL_SHORTS_NOT_FOUND(HttpStatus.NOT_FOUND,"SHORTS_002","쇼츠 리스트가 비어 있습니다."),
+    POPULAR_SHORTS_NOT_FOUND(HttpStatus.NOT_FOUND,"SHORTS_003","인기 쇼츠 리스트가 비어 있습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+}
