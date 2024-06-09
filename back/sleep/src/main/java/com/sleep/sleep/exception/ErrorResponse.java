9ce2d21833b836ca45655b3551b09521898b611c
ssafy10-dykim <dy.jo.kim@gmail.com>
2024-06-10 02:10:41 +0900

    Fix: 정적 팩토리 메소드 방식으로 변경

---
+++ back/sleep/src/main/java/com/sleep/sleep/exception/ErrorResponse.java
@@ -0,0 +1,39 @@
+package com.sleep.sleep.exception;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ErrorResponse {
+    private HttpStatus status;
+    private String code;
+    private String message;
+
+    @Builder
+    public ErrorResponse(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(ExceptionCode exceptionCode) {
+        return ErrorResponse.builder()
+                .code(exceptionCode.getCode())
+                .status(exceptionCode.getStatus())
+                .message(exceptionCode.getMessage())
+                .build();
+    }
+
+    public static ErrorResponse of(HttpStatus status, String message) {
+        return ErrorResponse.builder()
+                .code("COMMON")
+                .status(status)
+                .message(message)
+                .build();
+    }
+
+}
