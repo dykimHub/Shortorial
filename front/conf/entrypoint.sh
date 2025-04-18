57f63a245fedea96b484a5c0f567cb380b7e31a5
김다윤 <dy.jo.kim@gmail.com>
2025-04-18 15:29:17 +0900

    Chore: proxy pass 로그 출력

--- front/conf/entrypoint.sh
+++ front/conf/entrypoint.sh
@@ -2,4 +2,7 @@
 
 envsubst '${VITE_HOME_URL}' < /etc/nginx/templates/default.template.conf > /etc/nginx/conf.d/default.conf
 
+echo "proxy_pass: "
+cat /etc/nginx/conf.d/default.conf
+
 exec nginx -g 'daemon off;'
